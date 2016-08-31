6ba063306d42f91666f7f100671ef4bbabfd41d8
Jetroid <holtjet@gmail.com>
2016-08-31 12:32:57 +0100

    Add basic PDF functionality

--- assets/js/ihatereading.js
+++ assets/js/ihatereading.js
@@ -1,11 +1,26 @@
+function runPDF(url){
+	var url = 'https://jetroidcors.herokuapp.com/' + url;
+	PDFJS.workerSrc = './assets/js/pdf.worker.js';
+	var pdfjs2text = new PDFJS2TEXT();
+	pdfjs2text.pdfToText(
+		url, 
+		function(completed, total){ console.log ( completed + " / " + total); },
+		function(text) { run(text) }
+	);
+}
+
 function textCleanup(text){
-	var wikipediaCitations = /\[[0-9]+\]/g
+	var wikipediaCitations = /\[([0-9]+|edit)\]/g
 	return text.replace(wikipediaCitations, "")
 }
 
-
 function run(text){
-	var text = textCleanup(text);
+	if(text.endsWith(".pdf")){
+		runPDF(text);
+		return;
+	}
+
+	//var text = textCleanup(text);
 	var slideHolder = document.getElementById('slides');
 	
 	//Split our text into sentences.
@@ -30,13 +45,17 @@ function runOnEnter(e, textBox){
 	var key = e.which || e.keyCode;
 	if (key == 13 && !e.shiftKey) { // 13 is enter
 		e.preventDefault();
+		console.log(textBox.value);
 		run(textBox.value);
-  }
+	}
 }
 
 function addListeners(){
 	//On 'Enter', interpret the content of the textEntry box. 
 	document.getElementById("textEntry").addEventListener('keypress', function (e) {
+		runOnEnter(e, this);
+	});
+	document.getElementById("urlEntry").addEventListener('keypress', function(e) {
 		runOnEnter(e, this);
 	});
 }
